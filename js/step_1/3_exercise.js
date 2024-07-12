d59e99d31f9fb09b7adc8927ef6a69e3718f9d08
Calet Devia <calebtius@gmail.com>
2024-07-11 19:52:50 -0500

    Finished my third exercise

--- js/step_1/3_exercise.js
+++ js/step_1/3_exercise.js
@@ -1,7 +1,27 @@
 /*Ejercicio 3: Conversión de moneda
 Crea una función que solicite al usuario ingresar una cantidad en dólares y convierta esa cantidad a euros. Usa una tasa de conversión fija (por ejemplo, 1 USD = 0.85 EUR). Asegúrate de validar que el valor ingresado sea un número válido.*/
 
-function conversor(){
+function usdBecomeEur() {
+  let dolar = prompt("Writing your dollars here");
 
-    
+  if (dolar === null) {
+    alert("Cancel operation");
+    return;
+  }
+
+  dolar = dolar.trim();
+
+  if (!dolar) {
+    alert("Null or void value");
+  } else if (isNaN(dolar)) {
+    alert("Invalid value");
+  } else {
+    const usdToEur = 0.85;
+
+    alert("Your USD's in EURS is: $" + usdToEur * dolar);
+  }
+  return;
 }
+
+usdBecomeEur();
+
