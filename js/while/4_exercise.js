f425a092ec6347718adbf161e6ac7557e21e84c4
Calet Devia <calebtius@gmail.com>
2024-07-20 17:18:36 -0500

    Exercise 4 complete

---
+++ js/while/4_exercise.js
@@ -0,0 +1,14 @@
+/* Ejercicio 4: Tabla de Multiplicar
+Crea una función que muestre en la consola la tabla de multiplicar del 7 (del 7 x 1 al 7 x 10) usando un bucle while. */
+
+function tableOfSeven() {
+   
+  let otherNumber = 1;
+
+  while (otherNumber < 11) {
+    console.log(`\n ${7} X ${otherNumber} = ${7 * otherNumber}`);
+    otherNumber++;
+  }
+}
+
+tableOfSeven();
